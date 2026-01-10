2c149281c1b84cbb741a52718cc1618acabf7a61
CreamElDudJafar <jahern2398@gmail.com>
2026-01-10 08:21:53 -0600

    Update Core CPUSpeed Code

--- engine/battle/core.asm
+++ engine/battle/core.asm
@@ -6826,7 +6826,6 @@ DetermineWildOpponent:
 	callfar TryDoWildEncounter
 	ret nz
 InitBattleCommon:
-	callfar CGBSetCPU1xSpeed
 	ld a, [wMapPalOffset]
 	push af
 	ld hl, wLetterPrintingDelayFlags
@@ -6906,6 +6905,7 @@ InitWildBattle:
 
 ; common code that executes after init battle code specific to trainer or wild battles
 _InitBattleCommon:
+	callfar CGBSetCPU1xSpeed
 	ld b, SET_PAL_BATTLE_BLACK
 	call RunPaletteCommand
 	call SlidePlayerAndEnemySilhouettesOnScreen
